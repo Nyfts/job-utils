baeeb39181458662d4fd63f4187ba1745869985e
luan.teixeira <luan.teixeira@dock.tech>
2022-06-28 18:15:16 -0300

    spa init

---
+++ spa/src/Dashboard/index.tsx
@@ -0,0 +1,14 @@
+export function Dashboard() {
+  return (
+    <div className="container">
+      <header>
+        <h1>UTILS</h1>
+      </header>
+      <div className="menu">
+        <div className="menu-item">Daily</div>
+        <div className="menu-item">Menu dois</div>
+        <div className="menu-item">Menu três</div>
+      </div>
+    </div>
+  );
+}
